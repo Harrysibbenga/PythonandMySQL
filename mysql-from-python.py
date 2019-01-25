4c573a9b1548b62e402784fd17909fe3b7980e2d
Harry Sibbenga <sibbengaharry@gmail.com>
2019-01-25 21:04:49 +0000

    Added some data ro the Friends Table

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,10 +14,11 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS
-                          Friends(name char(20), age int, DOB datetime);""")
-        # Note that the above will still display a warning (not error) if the
-        # table already exists
+        rows = [("Bob", 21, "1990-02-06 23:04:56"),
+                ("Jim", 56, "1955-05-09 13:12:45"),
+                ("Fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%s);", rows)
+        connection.commit()
 finally:
     # Close the connection, regardless of whether or not the above was successful
     connection.close()
